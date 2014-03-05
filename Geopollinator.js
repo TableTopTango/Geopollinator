d5b50bb98c8f49ad8599392a95618c9b8e4d2055
piggy734 <philipcaffry@hanovernorwichschools.org>
2014-03-05 12:30:43 -0500

    Jem
    i just changed a few things it should still work

---
+++ Geopollinator.js
@@ -0,0 +1,113 @@
+//first create list on screen for clicking
+var table = document.getElementById("countries");
+for (i in worldmap.names) {
+    table.innerHTML+="<tr><td onclick='clickCountryCell(this)'>"+worldmap.names[i]+"</td></tr>";
+}
+
+var countriesClicked = [];
+
+//remove element from array
+function removeByValue(arr, val) {
+    for(var i=0; i<arr.length; i++) {
+        if(arr[i] == val) {
+            arr.splice(i, 1);
+            break;
+        }
+    }
+}
+
+function clickCountryCell(tableCell) {
+    var countryClicked = tableCell.innerHTML;
+    if (tableCell.className=="selectedCell") {
+        removeByValue(countriesClicked, countryClicked);
+        tableCell.className="deselectedCell";
+    } else {
+        countriesClicked.push(countryClicked);
+        tableCell.className="selectedCell";
+    }
+}
+
+var mouseIsDown = false;
+var prevMousePos;
+function mouseUp(event, obj) {
+    mouseIsDown=false;
+}
+
+function mouseDown(event, obj) {
+    mouseIsDown=true;
+    prevMousePos = [event.pageX-obj.offsetLeft, event.pageY-obj.offsetTop];
+}
+
+var position = {'x':0.0, 'y':0.0};
+function mouseMove(event, obj) {
+    if (mouseIsDown) {
+        var currentMousePos = [event.pageX-obj.offsetLeft, event.pageY-obj.offsetTop];
+        var xChange = currentMousePos[0]-prevMousePos[0];
+        var yChange = currentMousePos[1]-prevMousePos[1];
+        var scale = stage.scaleX();
+        position.x+=xChange;
+        position.y+=yChange;
+        stage.setPosition(position);
+        //mapLayer.draw();
+        //topLayer.draw();
+        prevMousePos=currentMousePos;
+    }
+}
+
+var ZOOM_FACTOR = .001;
+function scroll(event, obj) {
+    var currentScale = stage.scaleX();
+    var factor = ZOOM_FACTOR*Math.abs(event.wheelDelta)+1;
+    if (event.wheelDelta<0) {
+        factor = 1./factor;
+    }
+    var newScale = currentScale*factor;
+    var centerX = event.pageX-obj.offsetLeft;
+    var centerY = event.pageY-obj.offsetTop;
+    var currentX = position.x;
+    var currentY = position.y;
+    position = {'x':centerX+(currentX-centerX)*factor, 'y':centerY+(currentY-centerY)*factor};
+    stage.setPosition(position);
+    stage.scaleX(newScale);
+    stage.scaleY(newScale);
+    //stage.offsetX(currentX);
+    //stage.offsetY(currentY);
+    
+    mapLayer.draw();
+    topLayer.draw();
+}
+
+function mouseover(event, obj) {
+    document.body.style.overflow='hidden';
+}
+
+function mouseout(event, obj) {
+    document.body.style.overflow='auto';
+}
+
+var countryToFind;
+
+function startStudying() {
+    document.getElementById("countriesDiv").hidden=true;
+    //randomize country list
+    countriesClicked.sort(function() {return 0.5 - Math.random()});
+    console.log("start studying with countries:"+countriesClicked);
+    drawMapWithCountryList(countriesClicked);
+    
+    countryToFind=countriesClicked.pop();
+    document.getElementById("message").innerHTML="Click on "+countryToFind+".";
+}
+
+function checkCorrectCountry(countryName) {
+    if (countryName==countryToFind) {
+        //success
+        countryToFind=countriesClicked.pop();
+        if (countryToFind) {
+            document.getElementById("message").innerHTML="Good Job! You found "+countryName+". Now look for "+countryToFind+".";
+        } else {
+            document.getElementById("message").innerHTML="You found them all! Good job!";
+        }
+    } else {
+        document.getElementById("message").innerHTML="Nope. That is "+countryName+", not "+countryToFind+". Try again.";
+    }
+}
