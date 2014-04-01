339cff1b8138a0c7a8250686850cba7ea199744a
jakedickinson <jakedickinson@hanovernorwichschools.org>
2014-04-01 13:57:16 -0400

    Revert "Revert "Revert "Revert "hello""""
    This reverts commit acce9b2709102c362ee4bf373f6204def03b2a88.

--- Geopollinator.js
+++ Geopollinator.js
@@ -89,6 +89,7 @@ function mouseout(event, obj) {
 var i=0
 var countryToFind;
 var countryCount=0
+var wrongCount=0
 document.getElementById("replay").hidden=true;
 
 function startStudying() {
@@ -98,13 +99,16 @@ if(countryTotal>0){
     document.getElementById("message1").hidden=true;
     document.getElementById("message").hidden=false;
     document.getElementById("start").hidden=true;
+    document.getElementById("wrong").hidden=false;
     //randomize country list
     countriesClicked.sort(function() {return 0.5 - Math.random()});
     console.log("start studying with countries:"+countriesClicked);
     drawMapWithCountryList(countriesClicked);
 	document.getElementById("count").innerHTML=countryCount+" out of "+countryTotal
     countryToFind=countriesClicked[i];
-    document.getElementById("message").innerHTML="Click on "+countryToFind+".";}
+    document.getElementById("message").innerHTML="Click on "+countryToFind+".";
+    wrongCount=0
+    document.getElementById("wrong").innerHTML=wrongCount+" wrong answers."}
 }
 
 function checkCorrectCountry(countryName) {
@@ -123,6 +127,8 @@ function checkCorrectCountry(countryName) {
         }
     } else if(i<countriesClicked.length){
         document.getElementById("message").innerHTML="Nope. That is "+countryName+", not "+countryToFind+". Try again.";
+        wrongCount++
+        document.getElementById("wrong").innerHTML=wrongCount+" wrong answers."
     }
 }
 function playAgain(){document.getElementById("countriesDiv").hidden=false;
@@ -131,6 +137,7 @@ document.getElementById("replay").hidden=true;
 document.getElementById("start").hidden=false;
 document.getElementById("count").hidden=true;
 document.getElementById("message").hidden=true;
+document.getElementById("wrong").hidden=true;
 countryCount=0
 i=0
 }
