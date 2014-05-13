79754f6d58c3b6e8170af368e9dbeb06ebebb6c3
piggy734 <philipcaffry@hanovernorwichschools.org>
2014-05-13 13:21:58 -0400

    Converter

---
+++ Converter.js
@@ -0,0 +1,8 @@
+var converter = function(){
+	var convert = prompt("Degrees north, enter as negative if south.");
+	var convert2 = prompt("Degrees east, enter as negative if west.");
+	(convert + 90)(1000/360) = var newconvert;
+	(convert2 + 180)(1000/360) = var newconvert2;
+	document.write(newconvert);
+	document.write(newconvert2);
+}
