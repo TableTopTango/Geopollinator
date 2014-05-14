90214cfdf97dccd8975818b7c967689d7363f860
piggy734 <philipcaffry@hanovernorwichschools.org>
2014-05-14 12:34:06 -0400

    I fixed mything

--- Converter.js
+++ Converter.js
@@ -1,7 +1,7 @@
 function converter(){
 	var convert = parseFloat(document.getElementById("NorthSouth").value);
 	var convert2 = parseFloat(document.getElementById("EastWest").value);
-	var newconvert = (convert + 90)*(1000./360);
+	var newconvert = (convert + 90)*(500./180);
 	var newconvert2 = (convert2 + 180)*(1000./360);
 	document.writeln(newconvert);
 	
