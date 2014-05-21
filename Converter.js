7c6c94ca5b24cfc5e4bc1d16c27b11a8e18396fb
piggy734 <philipcaffry@hanovernorwichschools.org>
2014-05-21 12:33:16 -0400

    Fixed Converter

--- Converter.js
+++ Converter.js
@@ -3,7 +3,8 @@ function converter(){
 	var convert2 = parseFloat(document.getElementById("EastWest").value);
 	var newconvert = (convert + 90)*(500./180);
 	var newconvert2 = (convert2 + 180)*(1000./360);
-	document.writeln(newconvert);
-	
+
 	document.writeln(newconvert2);
+	
+	document.writeln(newconvert);
 }
