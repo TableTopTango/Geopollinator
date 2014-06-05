48753794392e0e30366e69e0b24a766ca36b96b0
piggy734 <philipcaffry@hanovernorwichschools.org>
2014-06-05 13:59:24 -0400

    hey

--- Converter.js
+++ Converter.js
@@ -1,10 +1,11 @@
 function converter(){
 	var convert = parseFloat(document.getElementById("NorthSouth").value);
 	var convert2 = parseFloat(document.getElementById("EastWest").value);
-	var newconvert = (convert + 90)*(500./180);
-	var newconvert2 = (convert2 + 180)*(1000./360);
+	var newconvert = (convert + 90)*(106.546/45.208834);
+	var newconvert2 = (convert2 + 180)*(284.434/112.846791);
 
 	document.writeln(newconvert2);
-	
+	document.writeln(",");
 	document.writeln(newconvert);
-}
+	document.writeln("L");
+};
